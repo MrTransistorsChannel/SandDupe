c166f5526f645e3c175729649cec9eb12014910f
MrTransistor <mrtransistor0108@gmail.com>
2021-08-21 15:26:37 +0300

    Initial commit

---
+++ src/ru/tulavcube/SandDupe/SandDupe.java
@@ -0,0 +1,74 @@
+package ru.tulavcube.SandDupe;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.FallingBlock;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+
+public class SandDupe extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getLogger().info("SandDupe plugin started");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        getServer().getLogger().info("SandDupe plugin stopped");
+    }
+
+    @EventHandler
+    public void onFallingBlockToBlock(EntityChangeBlockEvent e){
+        if(e.getEntityType() == EntityType.FALLING_BLOCK){
+            Entity entity = e.getEntity();
+            Location loc = entity.getLocation();
+            Vector vel = entity.getVelocity();
+            Block movingTo = getBlockMovingTo(loc, vel);
+
+            if(movingTo != null && movingTo.getType() == Material.END_PORTAL){
+                Location spawnLoc = movingTo.getLocation();
+                spawnLoc.setX(spawnLoc.getX()+0.5);
+                spawnLoc.setY(spawnLoc.getY()+0.5);
+                spawnLoc.setZ(spawnLoc.getZ()+0.5);
+                FallingBlock dummy = loc.getWorld().spawnFallingBlock(spawnLoc, ((FallingBlock) entity).getBlockData());
+                vel.setY(-vel.getY());
+                dummy.setVelocity(vel.multiply(2));
+            }
+        }
+    }
+
+    Block getBlockMovingTo(Location loc, Vector vel){
+        double absMax = 0, max = 0;
+        char dir = ' ';
+        Block relative = null;
+        if(Math.abs(vel.getX()) > absMax){
+            max = vel.getX();
+            absMax = Math.abs(vel.getX());
+            dir = 'x';
+        }
+        if(Math.abs(vel.getY()) > absMax){
+            max = vel.getY();
+            absMax = Math.abs(vel.getY());
+            dir = 'y';
+        }
+        if(Math.abs(vel.getZ()) > absMax){
+            max = vel.getZ();
+            dir = 'z';
+        }
+        switch (dir) {
+            case 'x' -> relative = loc.getBlock().getRelative((int) Math.signum(max), 0, 0);
+            case 'y' -> relative = loc.getBlock().getRelative(0, (int) Math.signum(max), 0);
+            case 'z' -> relative = loc.getBlock().getRelative(0, 0, (int) Math.signum(max));
+        }
+        return relative;
+    }
+}
